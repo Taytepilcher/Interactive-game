c4a9ccb7f58a63fb842a092d87116dce3b5bb719
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-11-15 12:49:50 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,54 +1,27 @@
-var widthRect = 50;
-var heightRect = 50;
-var radiusCircle= 50;
-
+var t = 0;
+var ellipsesize = 10;
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
     background(0);
+   
 }
 
 function draw() {
-   
+    background(0,5);
+
+  
+}
+function mouseMoved(){
+    var x = width * noise(t);
+    var y = height * noise(t+5);
 
     
-}
+    noStroke();
+    fill(255);
+    ellipse(x, y, ellipsesize);
+  ellipse(x+60, y, ellipsesize);
+    t = t + 0.01;
 
-function mousePressed(){
-    var rc0 = random(0,255);
-    var rc1 = random(0,255);
-    var rc2= random(0,255);
-if(mouseButton == LEFT){
-
-    fill(rc0, rc1, rc2);
-    rectMode(CENTER);
-    rect(mouseX, mouseY, widthRect, heightRect);
-}
-    else if (mouseButton == RIGHT){
-
-        fill (rc0, rc1, rc2);
-        ellipse(mouseX, mouseY, radiusCircle);
-    }
-
-}
-function keyPressed(){
-    var rc0 = random(0,255);
-    var rc1 = random(0,255);
-    var rc2= random(0,255);
-    if(keyCode == ENTER){
-        background(0);
-    }
-    if(keyCode == UP_ARROW){
-        fill(rc0,rc1,rc2)
-        triangle(mouseX, mouseY, mouseX+50, mouseY,mouseX+25, mouseY-50);
-    }
-    if(keyCode == LEFT_ARROW){
-        fill(rc0,rc1,rc2)
-        arc(mouseX, mouseY, 80, 80, 0, PI , PIE);
-    }
-    if(keyCode == RIGHT_ARROW){
-        fill(rc0,rc1,rc2)
-        arc(mouseX, mouseY, 80, 80, PI, 0 , PIE);
-    }
-    
+  
 }
